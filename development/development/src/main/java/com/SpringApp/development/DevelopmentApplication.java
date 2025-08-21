91ae5290ad83b68c8122b75e6b4fb7dda981fa3f
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2025-08-21 13:52:29 -0300

    Project created

---
+++ development/development/src/main/java/com/SpringApp/development/DevelopmentApplication.java
@@ -0,0 +1,16 @@
+package com.SpringApp.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentApplication.class, args);
+
+		System.out.println("Hello, Spring Boot!");
+
+	}
+
+}
